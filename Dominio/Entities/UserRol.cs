5d87f2eeec8b1ce9920328fa13ad093456e91b36
Marsh1100 <margie.b.agu17@gmail>
2023-09-06 15:25:11 -0500

    feat: :sparkles: Creacion de entitades y sus cardinalidades

---
+++ Dominio/Entities/UserRol.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio.Entities
+{
+    public class UserRol
+    {
+        public int IdUserFK { get; set; }
+        public User User { get; set; }
+        public int IdRolFK { get; set; }   
+        public Rol Rol { get; set; }
+    }
+}
