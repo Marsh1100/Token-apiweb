1c67d9de6ff3a6837e29e3e4b1f85832ad16c942
marsh1100 <margie.b.agu17@gmail.com>
2023-09-11 21:08:44 -0500

    Migracion

---
+++ Persistencia/Data/Migrations/20230911064514_InitialHome.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistencia.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialHome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "historialRefreshToken",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", maxLength: 10, nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IdUserFK = table.Column<int>(type: "int", nullable: false),
+                    token = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RefreshToken = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    fechaCreacion = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    fechaExpiracion = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    activo = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_historialRefreshToken", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_historialRefreshToken_User_IdUserFK",
+                        column: x => x.IdUserFK,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_historialRefreshToken_IdUserFK",
+                table: "historialRefreshToken",
+                column: "IdUserFK");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "historialRefreshToken");
+        }
+    }
+}
